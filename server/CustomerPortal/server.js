e9a93bdd83e837671517bb5bdc1215472486e978
mikhail chatillon <mikhail.chatillon@hotmail.com>
2018-03-22 10:27:30 +0100

    corrected customer portal + added outputs to the template

--- server/CustomerPortal/server.js
+++ server/CustomerPortal/server.js
@@ -9,36 +9,34 @@ app.use(express.static('static'));
 //recreate root certificate
 //arg rootname = the name of your root Certificate
 app.post('/postForm', function (req, res) {
+  var tags = JSON.parse(req.body.tags);
+  console.log(tags.tags);
+  var provisioningServiceClient = require('azure-iot-provisioning-service').ProvisioningServiceClient;
+  var registrationId = req.body.deviceId;
+  var serviceClient = provisioningServiceClient.fromConnectionString(process.env.CONNECTION_STRING);
 
-    
-        var provisioningServiceClient = require('azure-iot-provisioning-service').ProvisioningServiceClient;
-        var registrationId =  req.body.deviceId;
-        var serviceClient = provisioningServiceClient.fromConnectionString(process.env.CONNECTION_STRING);
-      
-        serviceClient.getIndividualEnrollment(registrationId, function (err, enrollmentResponse) {
-            if (err) {
-              console.log('error reading the individual enrollment: ' + err);
-            } else {
-              console.log("Etag: " + enrollmentResponse.etag);
-              
-              etag = enrollmentResponse.etag;
-    
-              enrollmentResponse.provisioningStatus = 'enabled';
-              enrollmentResponse.initialTwin = req.body.tags;
-              
-              serviceClient.createOrUpdateIndividualEnrollment(enrollmentResponse, function (err, enrollmentResponse2) {
-                if (err) {
-                  console.log('error updating the individual enrollment: ' + err);
-                } else {
-                  console.log("updated enrollment record returned: " + JSON.stringify(enrollmentResponse2, null, 2));
-                }
-              });
-    
-            }
-        });
-    
-        res.send('updated device enrolment for: ' + registrationId);
-        res.send();
-    });
+  serviceClient.getIndividualEnrollment(registrationId, function (err, enrollmentResponse) {
+    if (err) {
+      console.log('error reading the individual enrollment: ' + err);
+    } else {
+      console.log("Etag: " + enrollmentResponse.etag);
+      console.log(enrollmentResponse);
+      etag = enrollmentResponse.etag;
+      enrollmentResponse.initialTwin = tags;
+      enrollmentResponse.provisioningStatus = 'enabled';
+      serviceClient.createOrUpdateIndividualEnrollment(enrollmentResponse, function (err, enrollmentResponse2) {
+        if (err) {
+          console.log('error updating the individual enrollment: ' + err);
+        } else {
+          console.log("updated enrollment record returned: " + JSON.stringify(enrollmentResponse2, null, 2));
+        }
+      });
+
+    }
+  });
+
+  res.send('updated device enrolment for: ' + registrationId);
+  res.send();
+});
 
 http.createServer(app).listen(8000);
